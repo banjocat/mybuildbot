19be82174b0f8da2f641d36990e1a69cf2b34d87
banjocat <jackmuratore@gmail.com>
2017-03-05 13:05:16 -0500

    load env.sh with fabric

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,29 @@
-from fabric.api import local
+from fabric.api import local, with_settings
 
 workers = [
         'worker',
         ]
 
+try:
+    with open('./env.sh') as f:
+        environment = f.read()
+except IOError:
+    raise SystemExit("Must create env.sh first")
+
+@with_settings(prefix=environment)
 def up():
-    local("buildbot start master")
+    local("buildbot start master", shell='/bin/bash')
     for worker in workers:
-        local("buildbot-worker start %s" % worker)
+        local("buildbot-worker start %s" % worker, shell='/bin/bash')
 
 def reconfig():
     local("buildbot reconfig master")
 
 def down():
-    local("buildbot-worker stop worker")
+    local("buildbot stop master")
     for worker in workers:
         local("buildbot-worker stop %s" % worker)
+
+def log():
+    local("tail -f ./master/twistd.log")
+
